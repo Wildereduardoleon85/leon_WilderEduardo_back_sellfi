98542d0b3b36f913a5eac7b89f60ebf98bdd038a
Wilder Leon <wilder_eduardo85@hotmail.com>
2021-09-22 21:08:09 -0300

    added deply configuration

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, {Application, Request, Response} from 'express';
 import wheaterRoutes from './routes/wheater';
 import connectDB from './config/db';
+import path from 'path';
 
 const app: Application = express();
 
@@ -9,11 +10,17 @@ connectDB();
 app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
-    res.json({msg: "hello wordl!!"})
+    res.json({msg: "hello world!!"})
 })
 
 app.use('/api/v1/clima', wheaterRoutes);
 
-const port = process.env.PORT || 5000
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('./dist'))
+    app.get('*', (req, res)=> res.sendFile(path.resolve(__dirname, 'dist', 'index.html')))
+}
+
+const port = process.env.PORT || 8000
 
 app.listen(port, () => console.log(`Server running on port: ${port}`))
